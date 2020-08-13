0a3df5c953e3ed658c3d2192223d4be2190fd044
Adam King <adking710@gmail.com>
2020-08-13 14:11:52 -0500

    Inertial kermet

---
+++ routes/wiki.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.send('got to GET /wiki/')
+});
+
+router.post('/', (req, res, next) => {
+    res.send('got to GET /wiki/')
+});
+
+router.get('/add', (req, res, next) => {
+    res.send('got to GET /wiki/add')
+});
+
+module.exports = router;
